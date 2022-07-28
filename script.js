ac386e0b40f512580f1f6928d3bd5b9e7b2b3ee6
juncheolishere <90208306+juncheolishere@users.noreply.github.com>
2022-07-28 15:01:33 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const openMenu = document.getElementById("openMenu");
+const mainNav = document.getElementById("mainNav");
+const navBack = document.getElementById("navBack");
 const openM = () => {
-    openMenu.removeEventListener("click",openM);
-    openMenu.addEventListener("click",closeM);
+    
+    setTimeout(() => {
+        openMenu.addEventListener("click",closeM,{once : true})
+    },500)
 
     openMenu.children[0].style.top = "5px";
     openMenu.children[1].style.display = "none";
@@ -10,10 +14,15 @@ const openM = () => {
         openMenu.children[0].style.transform = "rotate(45deg)";
         openMenu.children[2].style.transform = "rotate(-45deg)";
     },250)
+
+    mainNav.style.right = "calc(0px)";
+    navBack.style.display = "flex";
+    document.body.style.overflowY = "hidden";
 }
 const closeM = () => {
-    openMenu.removeEventListener("click",closeM);
-    openMenu.addEventListener("click",openM);
+    setTimeout(() => {
+        openMenu.addEventListener("click",openM,{once : true})
+    },500)
 
     openMenu.children[0].style.transform = "rotate(0deg)";
     openMenu.children[1].style.display = "inline-block";
@@ -23,8 +32,13 @@ const closeM = () => {
         openMenu.children[0].style.top = "0px";
         openMenu.children[2].style.top = "10px";
     },250)
+
+    mainNav.style.right = "calc(-400px)";
+    navBack.style.display = "none";
+    document.body.style.overflowY = "visible";
+
 }
-openMenu.addEventListener("click",openM);
+openMenu.addEventListener("click",openM, {once : true});
 
 const trigger1 = document.getElementById("trigger1");
 const trigger2 = document.getElementById("trigger2");
@@ -84,228 +98,6 @@ let mainTyping= new TypeIt(".typeIt1", {
     .type("<em>- 당신을 위한 향수 15가지</em>")
     .go();
 
-const perfumeComment2 = document.getElementsByClassName("perfumeComment2");
-
-new TypeIt(perfumeComment2[0], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-  })
-    .type("'하얀색 벽과 푸른 지붕이 이어진 산토리니 해변,", { delay: 300 })
-    .pause(300)
-    .break()
-    .type("전망대에 다다라 바다를 본 순간")
-    .pause(300)
-    .break()
-    .type("누군가의 상쾌한 비누향기가 내 코 끝에 와닿는다.'")
-    .go();
-
-new TypeIt(perfumeComment2[1], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'한차례 소나기가 지나간 공원에서 느껴지는", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 공기중의 여유로운 이슬과 녹색 내음'")
-    .pause(300)
-    .break()
-    .go();
-
-new TypeIt(perfumeComment2[2], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'모래알을 잘게 부수며 철썩대는 파도 소리에", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 못 이기는 척 온몸을 던지게 만드는 시원함'")
-    .pause(300)
-    .break()
-    .go();
-
-new TypeIt(perfumeComment2[3], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'사라진 대륙 아틀란티스,", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 대륙을 감쪽같이 뒤덮은 대서양의 바다 내음'")
-    .pause(300)
-    .break()
-    .go();
-
-new TypeIt(perfumeComment2[4], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'고운 모래위에 설치된 푹신한 의자에 누워,", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 밀키스를 마시고 있는 풍경이 연상되는 향기'")
-    .pause(300)
-    .break()
-    .go();
-
-new TypeIt(perfumeComment2[5], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'따스한 햇살 아래보다", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 우중충한 회색 하늘 옅게 둘러싸인 안개 아래서")
-    .pause(300)
-    .break()
-    .type(" 더욱 선명한 생기를 뿜어내는 곳'")
-    .pause(300)
-    .break()
-    .go();
-
-new TypeIt(perfumeComment2[6], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'얼음장처럼 차가운 호수가 가득한 곳", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 호수위도 떠가는 하얀 구름이")
-    .pause(300)
-    .break()
-    .type(" 내 마음을 씻겨주는 듯한 시원하고 달콤한 향기'")
-    .pause(300)
-    .go();
-
-new TypeIt(perfumeComment2[7], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'푸른 지중해 바다가 기분 좋게 넘실거리고", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 그 위로 새하얀 요트가 바람을 천천히 가르면서")
-    .pause(300)
-    .break()
-    .type(" 여유와 풍경을 만끽하는 것 같은 향기'")
-    .pause(300)
-    .go();
-
-new TypeIt(perfumeComment2[8], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'녹색의 식물들로 가득한 숲이", { delay: 300 })
-    .pause(300)
-    .break()
-    .type("가끔은 푸르게 보일 수도 있구나 생각했다.")
-    .pause(300)
-    .break()
-    .type("그 푸른빛의 시린 공기가 바닥에 닿아")
-    .pause(300)
-    .break()
-    .type("꽃 처럼 부서지는 얼음'")
-    .go();
-
-new TypeIt(perfumeComment2[9], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'강렬하게 내리는 태양빛에 풍성하게 익은 오렌지,", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 오렌지의 묵직달콤함이 느껴지는 향'")
-    .pause(300)
-    .go();
-
-new TypeIt(perfumeComment2[10], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'고개를 돌려보니 갈대숲을 얼추 닮은 숲과 언덕,", { delay: 300 })
-    .pause(300)
-    .break()
-    .type("이국적인 편안함을 가져다 주는 자연 풍경'")
-    .pause(300)
-    .go();
-
-new TypeIt(perfumeComment2[11], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'수수함과 화사함의 중간", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 예쁜 청량 캐쥬얼 끝판왕'")
-    .pause(300)
-    .go();
-    
-new TypeIt(perfumeComment2[12], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'캐주얼한 복장과 늘 짓는 환한 미소로", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 어디에서나 인기가 많은 주목받는 사람'")
-    .pause(300)
-    .go();
-
-new TypeIt(perfumeComment2[13], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'함께 푸른 빛의 시원한 바람을 가로지를 때,", { delay: 300 })
-    .pause(300)
-    .break()
-    .type(" 뺨을 스치는 기분 좋은 신선함에")
-    .pause(300)
-    .break()
-    .type("저절로 지어지는 미소'")
-    .go();
-
-new TypeIt(perfumeComment2[14], {
-    speed: 50,
-    waitUntilVisible: true,
-    lifeLike: true,
-    cursor: false,
-    })
-    .type("'드럼세탁기에서 따듯한 바람으로", { delay: 300 })
-    .pause(300)
-    .break()
-    .type("살균까지 마치고 막 나온")
-    .pause(300)
-    .break()
-    .type("촉촉한 새하얀 호텔 수건'")
-    .pause(300)
-    .go();
 
 const trigger3 = document.getElementsByClassName("trigger3");
 const perfumeBox = document.getElementsByClassName("perfumeBox");
@@ -453,4 +245,51 @@ const nextPage2 = (obj) => {
         depth2 -= (listItemWidth+listMarginRight);
         perfumeList.style.transform = "translate("+depth2+"px)";
     }
+}
+
+
+const level1 = document.getElementsByClassName("level1");
+let lastOpen = null;
+
+const openC = (obj) => {
+    if (lastOpen)
+    {
+        lastOpen.click();
+    }
+
+    if (obj.parentNode.children.length == 1)
+    {
+        
+        obj.addEventListener('click',()=>{openC(obj)}, {once : true} )
+            
+        lastOpen=null;
+    }
+    else
+    {
+        const subCnt = obj.parentNode.children[1].children.length;
+        const heightValue = parseInt(getComputedStyle(obj).height.split('p')[0])+1;
+        obj.parentNode.style.minHeight = (heightValue*(subCnt+1))+"px"
+        obj.parentNode.children[1].style.top = heightValue+"px";
+        obj.parentNode.children[2].style.transform = "rotate(135deg)";
+        obj.parentNode.children[3].style.transform = "rotate(225deg)";
+        obj.addEventListener('click',()=>{closeC(obj)}, {once : true} )
+            
+        lastOpen = obj;
+    }
+
+}
+const closeC = (obj) => {
+    const heightValue = parseInt(getComputedStyle(obj).height.split('p')[0])+1;
+    obj.parentNode.style.minHeight = heightValue+"px"
+    obj.parentNode.children[1].style.top=-heightValue+"px";
+    obj.parentNode.children[2].style.transform = "rotate(0deg)";
+    obj.parentNode.children[3].style.transform = "rotate(90deg)";
+    obj.addEventListener('click',()=>{openC(obj)}, {once : true} )
+    
+    lastOpen = null;
+}
+
+for (let i = 0; i<level1.length; i++)
+{
+    level1[i].children[0].addEventListener('click',()=>{openC(level1[i].children[0])}, {once : true} )
 }
